e847c88c1e651871e1029df81c611fe547c3d3b6
abhi reddy <abhireddy@abhis-Air.attlocal.net>
2022-08-17 14:34:50 -0400

    added destroy_all action and cleaned up Item controller

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,57 +4,44 @@ class ItemsController < ApplicationController
   # GET /items or /items.json
   def index
     @items = Item.all
+    render json: @items
   end
 
   # GET /items/1 or /items/1.json
   def show
-  end
-
-  # GET /items/new
-  def new
-    @item = Item.new
-  end
-
-  # GET /items/1/edit
-  def edit
+    render json: @item
   end
 
   # POST /items or /items.json
   def create
     @item = Item.new(item_params)
 
-    respond_to do |format|
-      if @item.save
-        format.html { redirect_to item_url(@item), notice: "Item was successfully created." }
-        format.json { render :show, status: :created, location: @item }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @item.errors, status: :unprocessable_entity }
-      end
+    if @item.save
+      render json: @item, status: :ok
+    else
+      render json: @item.errors, status: :unprocessable_entity }
     end
   end
 
   # PATCH/PUT /items/1 or /items/1.json
   def update
-    respond_to do |format|
-      if @item.update(item_params)
-        format.html { redirect_to item_url(@item), notice: "Item was successfully updated." }
-        format.json { render :show, status: :ok, location: @item }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @item.errors, status: :unprocessable_entity }
-      end
+    if @item.update(item_params)
+      render json: @item, status: :ok
+    else
+      render json: @item.errors, status: :unprocessable_entity }
     end
   end
 
   # DELETE /items/1 or /items/1.json
   def destroy
     @item.destroy
+    render json: @item, status: :ok
+  end
 
-    respond_to do |format|
-      format.html { redirect_to items_url, notice: "Item was successfully destroyed." }
-      format.json { head :no_content }
-    end
+  def destroy_all
+    @items = Item.all
+    @items.destroy_all
+    render json: @items, status: :ok
   end
 
   private
